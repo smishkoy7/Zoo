4c81f257f0b5bbf7723704d5b1b4c2cadc825bc0
smishkoy <mishkoy2002@stud.op.edu.ua>
2024-01-16 20:42:04 +0200

    + choose department

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
@@ -9,5 +9,16 @@ public class Main {
         // 3. Посмотреть выбранного животного.
         // 4. Предложить посмотреть что-то еще.
         // 5. Выход из зоопарка.
+
+        System.out.println("Какой отдел зоопарка вы хотите посмотреть?");
+        Scanner scanner = new Scanner(System.in);
+
+        // Считываем введенную строку
+        int department = Integer.parseInt(scanner.nextLine());
+
+        // Выводим введенные данные
+        System.out.println("Вы выбрали отдел: " + department);
+        // Не забывайте закрывать Scanner, когда он больше не нужен
+        scanner.close();
     }
 }
