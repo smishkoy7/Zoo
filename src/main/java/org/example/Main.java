e54696088765e5f6c7437dbfcc24e733cb95f055
smishkoy <mishkoy2002@stud.op.edu.ua>
2024-01-16 21:16:10 +0200

    + main loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -11,30 +13,32 @@ public class Main {
         // 3. Посмотреть выбранного животного.
         // 4. Предложить посмотреть что-то еще.
         // 5. Выход из зоопарка.
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Какой отдел зоопарка вы хотите посмотреть?");
 
-        Map<Integer, String> departmens = new HashMap<Integer, String>();
-        departmens.put(0, "Уйти");
-        departmens.put(1, "Хищники");
-        departmens.put(2, "Птицы");
+        while (true) {
+            System.out.println("Какой отдел зоопарка вы хотите посмотреть?");
 
-        for(Map.Entry<Integer, String> item : departmens.entrySet()){
+            Map<Integer, String> departmens = new HashMap<Integer, String>();
+            departmens.put(0, "Уйти");
+            departmens.put(1, "Хищники");
+            departmens.put(2, "Птицы");
 
-            System.out.printf("%d - %s \n", item.getKey(), item.getValue());
-        }
-        // Считываем введенную строку
-        int department = Integer.parseInt(scanner.nextLine());
+            for (Map.Entry<Integer, String> item : departmens.entrySet()) {
+
+                System.out.printf("%d - %s \n", item.getKey(), item.getValue());
+            }
+            // Считываем введенную строку
+            int department = Integer.parseInt(scanner.nextLine());
+
+            if (department == 0) {
+                break;
+            } else {
+                // Выводим введенные данные
+                System.out.println("Вы выбрали отдел: " + department + " - " + departmens.get(department));
+            }
+        }//while
         scanner.close();
-
-        if(department == 0){
-            System.out.println("Вы покидаете наш зоопарк, всего доброго!");
-        } else{
-            // Выводим введенные данные
-            System.out.println("Вы выбрали отдел: " + department + " - " + departmens.get(department));
-        }
-
+        System.out.println("Вы покидаете наш зоопарк, всего доброго!");
 
     }
 }
