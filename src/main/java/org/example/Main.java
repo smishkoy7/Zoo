64b810bb15341f172a67702c868840756a9e8901
smishkoy <mishkoy2002@stud.op.edu.ua>
2024-01-16 21:04:01 +0200

    + list of departments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,6 @@
 package org.example;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -10,15 +12,29 @@ public class Main {
         // 4. Предложить посмотреть что-то еще.
         // 5. Выход из зоопарка.
 
-        System.out.println("Какой отдел зоопарка вы хотите посмотреть?");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Какой отдел зоопарка вы хотите посмотреть?");
 
+        Map<Integer, String> departmens = new HashMap<Integer, String>();
+        departmens.put(0, "Уйти");
+        departmens.put(1, "Хищники");
+        departmens.put(2, "Птицы");
+
+        for(Map.Entry<Integer, String> item : departmens.entrySet()){
+
+            System.out.printf("%d - %s \n", item.getKey(), item.getValue());
+        }
         // Считываем введенную строку
         int department = Integer.parseInt(scanner.nextLine());
-
-        // Выводим введенные данные
-        System.out.println("Вы выбрали отдел: " + department);
-        // Не забывайте закрывать Scanner, когда он больше не нужен
         scanner.close();
+
+        if(department == 0){
+            System.out.println("Вы покидаете наш зоопарк, всего доброго!");
+        } else{
+            // Выводим введенные данные
+            System.out.println("Вы выбрали отдел: " + department + " - " + departmens.get(department));
+        }
+
+
     }
 }
